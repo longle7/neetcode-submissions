a91e7cae8c98575e1294f8e910e61b1910dc4ec3
long <99153311+longle7@users.noreply.github.com>
2026-07-20 15:45:12 -0400

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.cs
@@ -0,0 +1,25 @@
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        int res = 0;
+        int left = 0;
+        var seen = new HashSet<char>();
+
+        for (int right = 0; right < s.Length; right++)
+        {
+            char c = s[right];
+
+            while (seen.Contains(c))
+            {
+                seen.Remove(s[left]);
+                left++;
+            }
+
+            seen.Add(c);
+            res = Math.Max(res, right - left +1);
+        }
+
+        return res;
+    }
+}
