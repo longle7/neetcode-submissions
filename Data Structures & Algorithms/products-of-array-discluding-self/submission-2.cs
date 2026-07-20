c083a4dadba9d282fcc1946f5739b5d2f334e6d2
long <99153311+longle7@users.noreply.github.com>
2026-07-20 12:32:35 -0400

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.cs
@@ -0,0 +1,23 @@
+public class Solution
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        int[] result = new int[nums.Length];
+
+        result[0] = 1;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            result[i] = nums[i - 1] * result[i - 1];
+        }
+
+        int suffix = 1;
+        for (int i = nums.Length -1; i >= 0; i--)
+        {
+            result[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return result;
+    }
+}
